ceea3ccd57a2278620f4f9667262ad025012b964
Jez <masteringthecode.woragis@gmail.com>
2023-04-13 02:01:53 -0300

    delet old database and use the models database, fixed room's page not showing the object name

--- base/views.py
+++ base/views.py
@@ -3,23 +3,12 @@ from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': '1', 'name': 'learning python'},
-    {'id': '2', 'name': 'lets learnjavascript'},
-    {'id': '3', 'name': 'suffering with java'},
-
-]
-
-
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
